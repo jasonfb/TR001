f141ceaf2a60af041f9f591935647f02bfa65f45
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2021-03-13 11:32:25 -0500

    removes code unrelated to the issue

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,13 +1,4 @@
 class HelloController < ApplicationController
-
-
-  def anything
-    respond_to do |format|
-      format.turbo_stream {redirect_to "/nowhere"}
-      format.html
-    end
-  end
-
   def world
 
   end
